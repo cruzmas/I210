fb646ca0bdd5d2d1ef59c36cafbb4c233c17d88c
Mason <cruzmas@iu.edu>
2024-10-08 11:27:06 -0400

    Made list of JavaScript code

---
+++ Week7/console.js
@@ -0,0 +1,35 @@
+console.log("hi")
+
+let myAge = 52;
+myAge = 53;
+
+console.log(myAge);
+
+let myDog = {
+    hairColor: "green",
+    length: "10ft",
+    teethCount: 274,
+    name: "Franklin", 
+};
+
+console.log(myDog);
+console.log("Dog Name", myDog.name)
+console.log("Dog Friends", myDog.friends)
+
+/*
+Create Variables for the following
+-your name
+-your favorite number
+-whether you came to class today
+-Describe a monster, at least three features
+*/
+
+const myName = "Mason";
+let favoriteNumber = 3;
+let attendedClass = true;
+let myMonster = {
+    height: "15 feet",
+    teeth: 350,
+    color: "red",
+    smellsBad: true,
+};
