f6be1d49839b1a9154ebf80af5fdf511905f154f
Mason <cruzmas@iu.edu>
2024-11-05 11:50:32 -0500

    Created Week 11 folder and files

---
+++ Week11/digimon.js
@@ -0,0 +1,10 @@
+async function getDigimon() {
+    const digiResponse = await fetch(
+        `https://digimon-api.vercel.app/api/digimon`
+    );
+    const digiData = await digiResponse.json();
+    console.log(digiData);
+
+    return digiData;
+    
+}
