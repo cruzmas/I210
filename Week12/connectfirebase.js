4c6f63761ec7114cc49f250fc3760ec711f71be9
Mason <cruzmas@iu.edu>
2024-11-14 11:28:34 -0500

    Added list and button

--- Week12/connectfirebase.js
+++ Week12/connectfirebase.js
@@ -1,5 +1,5 @@
 import { app } from "./firebase.js";
-import{getFirestore, collection, getDocs,} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"; 
+import{getFirestore, collection, getDocs, addDoc,} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"; 
 
 const booksListRef = document.querySelector("#booksList")
 
@@ -9,13 +9,28 @@ async function getBooks() {
     const booksCollection = collection(db, "books");
     const bookDocs = await getDocs(booksCollection);
 
+
+booksListRef.innerHTML = "";
 for (let i = 0; i < bookDocs.docs.length; i++) {
     const books = bookDocs.docs[i];
     const bookData = books.data();
 
-    booksListRef.innerHTML += `<li>${bookData.title} (${bookData.yearPublished})</li>`
+    booksListRef.innerHTML += `<li>${bookData.title} (${bookData.yearPublished})</li>`;
 
 }
 }
+
+async function addBook() {
+    const bookName = prompt("Book Name:");
+    const yearPublished = prompt("Year Published: ");
+
+    const booksCollection = collection(db, "books");
+
+    await addDoc(booksCollection, { title: bookName, yearPublished: yearPublished });
+
+    getBooks();
+}
+
+document.querySelector("#addBookButton").onclick = addBook;
 
 getBooks();
